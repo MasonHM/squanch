e867ee0015471b74be95efb3fd9ac34ebcfcacad
Mason Meier <masonhm@gmail.com>
2023-11-09 13:11:22 -0800

    Temporarily randomize my weight

--- app/api/combined/route.ts
+++ app/api/combined/route.ts
@@ -1,7 +1,10 @@
+import { CombinedData } from "@/lib/storage/data";
 import { getAllData } from "@/lib/storage/google-sheets";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest): Promise<NextResponse> {
   const dummy = req.body; // treat this API as dynamic to update data
-  return NextResponse.json(await getAllData());
+  const data: CombinedData = await getAllData();
+  data.weightData["Mason"] = Math.round(Math.random() * 100) + 100;
+  return NextResponse.json(data);
 }
