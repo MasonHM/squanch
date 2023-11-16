0a5f3bef40e832109965a88d7857fd8b9c881d9e
Mason Meier <masonhm@gmail.com>
2023-11-16 15:06:11 -0800

    Show FCM token for debugging

--- components/notifications/subscribe-button.tsx
+++ components/notifications/subscribe-button.tsx
@@ -3,10 +3,13 @@ import Image from "next/image";
 import Link from "next/link";
 import styles from "./subscribe-button.module.scss";
 import { useEffect, useState } from "react";
+import NotificationModal from "./notification-modal";
+import { Update } from "@/lib/storage/data";
 
 export default function SubscribeButton() {
   const [subscribed, setSubscribed] = useState(false);
   const [notificationsSupported, setNotificationsSupported] = useState(false);
+  const [modalUpdates, setModalUpdates] = useState<Update[]>([]);
 
   useEffect(() => {
     const checkFirebaseSupport = async () => {
@@ -24,26 +27,46 @@ export default function SubscribeButton() {
     }
   }, [notificationsSupported]);
 
-  return subscribed || !notificationsSupported ? (
+  const updateToken = () => {
+    const token = localStorage.getItem("fcm-token") || "No token found";
+    console.log("FCM Token: " + token);
+    setModalUpdates([{ title: "FCM Token", body: token }]);
+  };
+
+  return !notificationsSupported ? (
     <></>
   ) : (
-    <div className={styles.wrapper}>
-      <div className={styles.subscribe}>
-        <Link href="#">
-          <Image
-            src="/bell.png"
-            alt="Think outside the bun."
-            fill={true}
-            className={styles.button}
-            onClick={async () => {
-              const permissionState = await requestNotificationPermissions();
-              if (permissionState === "granted") {
-                setSubscribed(true);
+    <>
+      <div className={styles.wrapper}>
+        <div className={styles.subscribe}>
+          <Link href="#">
+            <Image
+              src="/bell.png"
+              alt="Think outside the bun."
+              fill={true}
+              className={styles.button}
+              onClick={
+                subscribed
+                  ? () => {
+                      updateToken();
+                    }
+                  : async () => {
+                      const permissionState = await requestNotificationPermissions();
+                      if (permissionState === "granted") {
+                        setSubscribed(true);
+                      }
+                    }
               }
-            }}
-          />
-        </Link>
+            />
+          </Link>
+        </div>
       </div>
-    </div>
+      <NotificationModal
+        updates={modalUpdates}
+        exitCallback={() => {
+          setModalUpdates([]);
+        }}
+      />
+    </>
   );
 }
